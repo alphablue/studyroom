598bf7861b504037eb26375d5be39aa1cf5c3ae8
sjLee <imy123123@naver.com>
2022-08-08 16:48:47 +0900

    [add: home_screen]  티켓을 붉은 부분 구분선 만들기

--- android/flutter/startBasic/lib/screens/ticket_view.dart
+++ android/flutter/startBasic/lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:startbasic/utils/app_layout.dart';
 import 'package:startbasic/utils/app_styles.dart';
 import 'package:startbasic/widgets/ticket_container.dart';
@@ -17,6 +18,9 @@ class TicketView extends StatelessWidget {
         margin: const EdgeInsets.only(left: 16),
         child: Column(
           children: [
+            /**
+             * 티켓의 파란부분을 보여주는 뷰
+             * */
             Container(
               decoration: const BoxDecoration(
                 color: Color(0xFF526799),
@@ -29,42 +33,144 @@ class TicketView extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      Text("NYC", style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      Text(
+                        "NYC",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      ),
                       const Spacer(),
                       TicketContainer(),
-                      Expanded(child: Stack(
-                        children: [
-                          SizedBox(
-                            /**
-                             *  "-" 가 화면의 크기에 따라서 동적으로 보여주기 위해서 화면의 크기를
-                             *  알아야 한다. 그것을 위해서 LayoutBuilder 로 감싸준 것
-                             * */
-                            height:24,
-                            child: LayoutBuilder(
-                              builder: (BuildContext context, BoxConstraints constraints) {
-                                print("The width is ${constraints.constrainWidth()}");
-                                return Flex(
-                                  direction: Axis.horizontal,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: List.generate((constraints.constrainWidth()/6).floor(), (index) => const SizedBox(
-                                    width: 3,
-                                    height: 1,
-                                    child: DecoratedBox(decoration: BoxDecoration(
-                                        color: Colors.white
-                                    ),),
-                                  )),
-                                );
-                              },
-                            ),
+                      Expanded(
+                          child: Stack(children: [
+                        SizedBox(
+                          /**
+                               *  "-" 가 화면의 크기에 따라서 동적으로 보여주기 위해서 화면의 크기를
+                               *  알아야 한다. 그것을 위해서 LayoutBuilder 로 감싸준 것
+                               * */
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              print(
+                                  "The width is ${constraints.constrainWidth()}");
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                children: List.generate(
+                                    (constraints.constrainWidth() / 6).floor(),
+                                    (index) => const SizedBox(
+                                          width: 3,
+                                          height: 1,
+                                          child: DecoratedBox(
+                                            decoration: BoxDecoration(
+                                                color: Colors.white),
+                                          ),
+                                        )),
+                              );
+                            },
                           ),
-                          Center(child: Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white,)))
-                        ]
-                      )), // Spacer 와 Expanded(child: Container()) 은 같음
+                        ),
+                        Center(
+                            child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                )))
+                      ])), // Spacer 와 Expanded(child: Container()) 은 같음
                       TicketContainer(),
                       const Spacer(),
-                      Text("London", style: Styles.headLineStyle3.copyWith(color: Colors.white),)
+                      Text(
+                        "LDN",
+                        style:
+                            Styles.headLineStyle3.copyWith(color: Colors.white),
+                      )
                     ],
+                  ),
+                  const Gap(1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'New-York',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                      Text(
+                        "8H 30M",
+                        style:
+                            Styles.headLineStyle4.copyWith(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'London',
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            /**
+             * 티켓의 붉은 부분을 보여주는 뷰
+             * */
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              bottomRight: Radius.circular(10))),
+                    ),
+                  ),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: List.generate(
+                              (constraints.constrainWidth() / 15).floor(),
+                              (index) => const SizedBox(
+                                    width: 5,
+                                    height: 1,
+                                    child: DecoratedBox(
+                                      decoration:
+                                          BoxDecoration(color: Colors.white),
+                                    ),
+                                  )),
+                        );
+                      },
+                    ),
+                  )),
+                  SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10))),
+                    ),
                   )
                 ],
               ),

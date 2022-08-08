065da14ceedb4c69ecdb7b3f4eacfd5d01715daf
sjLee <imy123123@naver.com>
2022-08-08 14:59:46 +0900

    [add]  홈 화면 검색 창 만들기

--- android/flutter/startBasic/lib/screens/home_screen.dart
+++ android/flutter/startBasic/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -17,6 +18,7 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -50,7 +52,22 @@ class HomeScreen extends StatelessWidget {
                       ),
                     )
                   ],
-                )
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xfff4f6fd)
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xffbfc205)),
+                      Text("Search", style: Styles.headLineStyle4,)
+                    ],
+                  ),
+                ),
+
               ],
             ),
           )

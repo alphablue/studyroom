2823f561fa52e781e59045fb0dfd7627e2403643
sjLee <imy123123@naver.com>
2022-08-08 16:11:44 +0900

    [add: home_screen]  한쪽으로 치우친 비행기 모양을 가운데로 정렬함

--- android/flutter/startBasic/lib/screens/ticket_view.dart
+++ android/flutter/startBasic/lib/screens/ticket_view.dart
@@ -32,31 +32,35 @@ class TicketView extends StatelessWidget {
                       Text("NYC", style: Styles.headLineStyle3.copyWith(color: Colors.white),),
                       const Spacer(),
                       TicketContainer(),
-                      Expanded(child: SizedBox(
-                        /**
-                         *  "-" 가 화면의 크기에 따라서 동적으로 보여주기 위해서 화면의 크기를
-                         *  알아야 한다. 그것을 위해서 LayoutBuilder 로 감싸준 것
-                         * */
-                        height:24,
-                        child: LayoutBuilder(
-                          builder: (BuildContext context, BoxConstraints constraints) {
-                            print("The width is ${constraints.constrainWidth()}");
-                            return Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              mainAxisSize: MainAxisSize.max,
-                              children: List.generate((constraints.constrainWidth()/6).floor(), (index) => const SizedBox(
-                                width: 3,
-                                height: 1,
-                                child: DecoratedBox(decoration: BoxDecoration(
-                                  color: Colors.white
-                                ),),
-                              )),
-                            );
-                          },
-                        ),
+                      Expanded(child: Stack(
+                        children: [
+                          SizedBox(
+                            /**
+                             *  "-" 가 화면의 크기에 따라서 동적으로 보여주기 위해서 화면의 크기를
+                             *  알아야 한다. 그것을 위해서 LayoutBuilder 로 감싸준 것
+                             * */
+                            height:24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context, BoxConstraints constraints) {
+                                print("The width is ${constraints.constrainWidth()}");
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate((constraints.constrainWidth()/6).floor(), (index) => const SizedBox(
+                                    width: 3,
+                                    height: 1,
+                                    child: DecoratedBox(decoration: BoxDecoration(
+                                        color: Colors.white
+                                    ),),
+                                  )),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(child: Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white,)))
+                        ]
                       )), // Spacer 와 Expanded(child: Container()) 은 같음
-                      Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white,)),
                       TicketContainer(),
                       const Spacer(),
                       Text("London", style: Styles.headLineStyle3.copyWith(color: Colors.white),)

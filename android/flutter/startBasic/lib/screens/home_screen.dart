ef1ec5a60456c8557de667f0af92e3750a535060
sjLee <imy123123@naver.com>
2022-08-08 14:27:56 +0900

    [add, refactor]  홈 화면의 상단 문구와 이미지 표현하기,  이미지를 사용하기 위한 디렉토리 설정 하기

---
+++ android/flutter/startBasic/lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: const [
+                        Text("Good Morning"),
+                        Text("Book Tickets")
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage(
+                            "assets/images/img_1.png"
+                          )
+                        )
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

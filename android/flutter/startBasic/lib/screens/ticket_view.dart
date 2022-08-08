30d8b834078571a35551b3685379c5ecfffaebfc
sjLee <imy123123@naver.com>
2022-08-08 16:10:17 +0900

    [add: home_screen]  티켓에서 비행기 모양이 나오도록 조정함 그런데 비행이가 한쪽에 치우치는 현상이 있음  이것을 stack 구조를 활용해 가운데로 옮길 것이다.

--- android/flutter/startBasic/lib/screens/ticket_view.dart
+++ android/flutter/startBasic/lib/screens/ticket_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:startbasic/utils/app_layout.dart';
 import 'package:startbasic/utils/app_styles.dart';
+import 'package:startbasic/widgets/ticket_container.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
@@ -29,6 +30,34 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text("NYC", style: Styles.headLineStyle3.copyWith(color: Colors.white),),
+                      const Spacer(),
+                      TicketContainer(),
+                      Expanded(child: SizedBox(
+                        /**
+                         *  "-" 가 화면의 크기에 따라서 동적으로 보여주기 위해서 화면의 크기를
+                         *  알아야 한다. 그것을 위해서 LayoutBuilder 로 감싸준 것
+                         * */
+                        height:24,
+                        child: LayoutBuilder(
+                          builder: (BuildContext context, BoxConstraints constraints) {
+                            print("The width is ${constraints.constrainWidth()}");
+                            return Flex(
+                              direction: Axis.horizontal,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisSize: MainAxisSize.max,
+                              children: List.generate((constraints.constrainWidth()/6).floor(), (index) => const SizedBox(
+                                width: 3,
+                                height: 1,
+                                child: DecoratedBox(decoration: BoxDecoration(
+                                  color: Colors.white
+                                ),),
+                              )),
+                            );
+                          },
+                        ),
+                      )), // Spacer 와 Expanded(child: Container()) 은 같음
+                      Transform.rotate(angle: 1.5, child: const Icon(Icons.local_airport_rounded, color: Colors.white,)),
+                      TicketContainer(),
                       const Spacer(),
                       Text("London", style: Styles.headLineStyle3.copyWith(color: Colors.white),)
                     ],

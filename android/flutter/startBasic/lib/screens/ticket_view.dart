1520310a6ccd6a2553837acc103e72d28a245c5e
sjLee <imy123123@naver.com>
2022-08-11 14:20:08 +0900

    [refactor: app_layout.dart]  다양한 기기에 대응 하기 위한 화면 정보를 받아오고 픽셀화된 크기를 비율에 맞춘것

--- android/flutter/startBasic/lib/screens/ticket_view.dart
+++ android/flutter/startBasic/lib/screens/ticket_view.dart
@@ -19,22 +19,22 @@ class TicketView extends StatelessWidget {
        * 것이 더 직관적이다.
        * */
       width: size.width * 0.85,
-      height: 200,
+      height: AppLayout.getHeight(200),
       child: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
         child: Column(
           children: [
             /**
              * 티켓의 파란부분을 보여주는 뷰
              * */
             Container(
-              decoration: const BoxDecoration(
+              decoration:  BoxDecoration(
                 color: Color(0xFF526799),
                 borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(21),
-                    topRight: Radius.circular(21)),
+                    topLeft: Radius.circular(AppLayout.getHeight(21)),
+                    topRight: Radius.circular(AppLayout.getHeight(21))),
               ),
-              padding: const EdgeInsets.all(16),
+              padding: EdgeInsets.all(AppLayout.getHeight(16)),
               child: Column(
                 children: [
                   Row(
@@ -66,10 +66,10 @@ class TicketView extends StatelessWidget {
                                 mainAxisSize: MainAxisSize.max,
                                 children: List.generate(
                                     (constraints.constrainWidth() / 6).floor(),
-                                    (index) => const SizedBox(
-                                          width: 3,
-                                          height: 1,
-                                          child: DecoratedBox(
+                                    (index) => SizedBox(
+                                          width: AppLayout.getWidth(3),
+                                          height: AppLayout.getHeight(1),
+                                          child: const DecoratedBox(
                                             decoration: BoxDecoration(
                                                 color: Colors.white),
                                           ),
@@ -100,7 +100,7 @@ class TicketView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['from']['name'],
                           style: Styles.headLineStyle4
@@ -113,7 +113,7 @@ class TicketView extends StatelessWidget {
                             Styles.headLineStyle4.copyWith(color: Colors.white),
                       ),
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['to']['name'],
                           textAlign: TextAlign.end,
@@ -133,15 +133,15 @@ class TicketView extends StatelessWidget {
               color: Styles.orangeColor,
               child: Row(
                 children: [
-                  const SizedBox(
-                    height: 20,
-                    width: 10,
+                  SizedBox(
+                    height: AppLayout.getHeight(20),
+                    width: AppLayout.getWidth(10),
                     child: DecoratedBox(
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10))),
+                              topRight: Radius.circular(AppLayout.getHeight(10)),
+                              bottomRight: Radius.circular(AppLayout.getHeight(10)))),
                     ),
                   ),
                   Expanded(
@@ -156,10 +156,10 @@ class TicketView extends StatelessWidget {
                           mainAxisSize: MainAxisSize.max,
                           children: List.generate(
                               (constraints.constrainWidth() / 15).floor(),
-                              (index) => const SizedBox(
-                                    width: 5,
-                                    height: 1,
-                                    child: DecoratedBox(
+                              (index) => SizedBox(
+                                    width: AppLayout.getWidth(5),
+                                    height: AppLayout.getHeight(1),
+                                    child: const DecoratedBox(
                                       decoration:
                                           BoxDecoration(color: Colors.white),
                                     ),
@@ -168,15 +168,15 @@ class TicketView extends StatelessWidget {
                       },
                     ),
                   )),
-                  const SizedBox(
-                    height: 20,
-                    width: 10,
+                  SizedBox(
+                    height: AppLayout.getHeight(20),
+                    width: AppLayout.getWidth(10),
                     child: DecoratedBox(
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomLeft: Radius.circular(10))),
+                              topLeft: Radius.circular(AppLayout.getHeight(10)),
+                              bottomLeft: Radius.circular(AppLayout.getHeight(10)))),
                     ),
                   )
                 ],
@@ -188,9 +188,9 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                   color: Styles.orangeColor,
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(21),
-                      bottomRight: Radius.circular(21))),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(AppLayout.getHeight(21)),
+                      bottomRight: Radius.circular(AppLayout.getHeight(21)))),
               padding: const EdgeInsets.only(left: 16, top: 10, right: 16, bottom: 16),
               child: Column(
                 children: [
--- android/flutter/startBasic/lib/utils/app_layout.dart
+++ android/flutter/startBasic/lib/utils/app_layout.dart
@@ -6,5 +6,24 @@ class AppLayout {
     return MediaQuery.of(context).size;
   }
 
-  static getScreenHeight
+  static getScreenHeight() {
+    return Get.height;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
+  }
+
+  /// 화면의 크기에 따라서 비율이 달라지기 때문에
+  /// 그 비율에 맞는 픽셀을 가져오도록 함함
+ ///
+  static getHeight(double pixels) {
+    double x = getScreenHeight() / pixels; // 844/200 => 4.22
+    return getScreenHeight() / x;
+  }
+
+  static getWidth(double pixels) {
+    double x = getScreenWidth() / pixels;
+    return getScreenWidth() / x;
+  }
 }

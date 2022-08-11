0767ed0bb81f007b332005ea8a45e5c3827ec991
sjLee <imy123123@naver.com>
2022-08-11 16:57:18 +0900

    [add: search_screen]  검색 화면의 아래에 있는 오른쪽 2개 박스 만들기

--- android/flutter/startBasic/lib/screens/search_screen.dart
+++ android/flutter/startBasic/lib/screens/search_screen.dart
@@ -18,22 +18,22 @@ class SearchScreen extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.symmetric(
             horizontal: AppLayout.getWidth(20),
-            vertical: AppLayout.getHeight(20)
-        ),
+            vertical: AppLayout.getHeight(20)),
         children: [
           Gap(AppLayout.getHeight(40)),
           Text(
             "What are\n you looking for?",
-            style: Styles.headLineStyle1.copyWith(fontSize: AppLayout.getWidth(35)), //fontSize: 35 를 width 사이즈로 조정이 가능하다
+            style: Styles.headLineStyle1.copyWith(
+                fontSize: AppLayout.getWidth(
+                    35)), //fontSize: 35 를 width 사이즈로 조정이 가능하다
           ),
           Gap(AppLayout.getHeight(20)),
           FittedBox(
             child: Container(
               padding: const EdgeInsets.all(3.5),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
-                color: const Color(0xFFf4f6fd)
-              ),
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                  color: const Color(0xFFf4f6fd)),
               child: Row(
                 children: [
                   /**
@@ -41,14 +41,16 @@ class SearchScreen extends StatelessWidget {
                    * */
                   Container(
                     width: size.width * 0.44,
-                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.horizontal( //horizontal 을 넣으면 좌, 우 중 하나에게 radius 값을 줄 수 있음
-                            left: Radius.circular(AppLayout.getHeight(50))
-                        ),
-                        color: Colors.white
+                        borderRadius: BorderRadius.horizontal(
+                            //horizontal 을 넣으면 좌, 우 중 하나에게 radius 값을 줄 수 있음
+                            left: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.white),
+                    child: Center(
+                      child: Text("Airline Tickets"),
                     ),
-                    child: Center(child: Text("Airline Tickets"),),
                   ),
 
                   /**
@@ -56,17 +58,19 @@ class SearchScreen extends StatelessWidget {
                    * */
                   Container(
                     width: size.width * 0.44,
-                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.horizontal( //horizontal 을 넣으면 좌, 우 중 하나에게 radius 값을 줄 수 있음
-                            right: Radius.circular(AppLayout.getHeight(50))
-                        ),
+                        borderRadius: BorderRadius.horizontal(
+                            //horizontal 을 넣으면 좌, 우 중 하나에게 radius 값을 줄 수 있음
+                            right: Radius.circular(AppLayout.getHeight(50))),
                         /**
                          * 상위 컨테이너의 색을 그대로 사용하기 위해서 transparent 를 사용함
                          * */
-                        color: Colors.transparent
+                        color: Colors.transparent),
+                    child: Center(
+                      child: Text("Hotel"),
                     ),
-                    child: Center(child: Text("Hotel"),),
                   ),
                 ],
               ),
@@ -78,54 +82,52 @@ class SearchScreen extends StatelessWidget {
           AppIconText(icon: Icons.flight_land_rounded, text: "Arrival"),
           Gap(AppLayout.getHeight(25)),
           Container(
-            padding: EdgeInsets.symmetric(vertical: AppLayout.getWidth(18), horizontal: AppLayout.getWidth(15)),
-            decoration: BoxDecoration(
-              color: Color(0xd91130ce),
-              borderRadius: BorderRadius.circular(AppLayout.getWidth(10)),
-            ),
-            child: Center(
-              child: Text(
-                "find tickets",
-                style: Styles.textStyle.copyWith(color: Colors.white),
+              padding: EdgeInsets.symmetric(
+                  vertical: AppLayout.getWidth(18),
+                  horizontal: AppLayout.getWidth(15)),
+              decoration: BoxDecoration(
+                color: Color(0xd91130ce),
+                borderRadius: BorderRadius.circular(AppLayout.getWidth(10)),
               ),
-            )
-          ),
+              child: Center(
+                child: Text(
+                  "find tickets",
+                  style: Styles.textStyle.copyWith(color: Colors.white),
+                ),
+              )),
           Gap(AppLayout.getHeight(40)),
-          const AppDoubleTextWidget(bigText: 'Upcoming Flights', smallText: 'View all'),
+          const AppDoubleTextWidget(
+              bigText: 'Upcoming Flights', smallText: 'View all'),
           Gap(AppLayout.getHeight(15)),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                height: AppLayout.getHeight(400),
+                height: AppLayout.getHeight(425),
                 width: size.width * 0.42,
                 padding: EdgeInsets.symmetric(
                     horizontal: AppLayout.getHeight(15),
-                    vertical: AppLayout.getWidth(15)
-                ),
+                    vertical: AppLayout.getWidth(15)),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(AppLayout.getHeight(20)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      blurRadius: 1,
-                      spreadRadius: 1
-                    )
-                  ]
-                ),
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(20)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade200,
+                          blurRadius: 1,
+                          spreadRadius: 1)
+                    ]),
                 child: Column(
                   children: [
                     Container(
                       height: AppLayout.getHeight(190),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(AppLayout.getHeight(12)),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            "assets/images/sit.jpg"
-                          )
-                        )
-                      ),
+                          borderRadius:
+                              BorderRadius.circular(AppLayout.getHeight(12)),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/sit.jpg"))),
                     ),
                     Gap(AppLayout.getHeight(12)),
                     Text(
@@ -134,6 +136,99 @@ class SearchScreen extends StatelessWidget {
                     )
                   ],
                 ),
+              ),
+              Column(
+                children: [
+                  /**
+                   * 플러터에서 스택은 다양한 UI를 만들기위해서 사용되는 방법임 position 조정도 가능하고
+                   * 자료를 찾아볼 필요가 있음
+                   * */
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(200),
+                        decoration: BoxDecoration(
+                            color: Color(0xff3ab8b8),
+                            borderRadius:
+                                BorderRadius.circular(AppLayout.getHeight(18))),
+                        padding: EdgeInsets.symmetric(
+                            vertical: AppLayout.getHeight(15),
+                            horizontal: AppLayout.getHeight(15)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Discount\nfor survey",
+                                style: Styles.headLineStyle2.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            Gap(AppLayout.getHeight(10)),
+                            Text(
+                                "Take the survey about our services and get discount",
+                                style: Styles.headLineStyle2.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white,
+                                    fontSize: 18)),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -40,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  width: 18, color: Color(0xff189999)),
+                              color: Colors.transparent),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(15)),
+                  Container(
+                    width: size.width * 0.44,
+                    height: AppLayout.getHeight(210),
+                    padding: EdgeInsets.symmetric(
+                        vertical: AppLayout.getHeight(15),
+                        horizontal: AppLayout.getHeight(15)),
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(18)),
+                        color: const Color(0xffec6545)),
+                    /**
+                     * 이모티콘 넣기
+                     */
+                    child: Column(
+                      children: [
+                        Text(
+                          'Take love',
+                          style: Styles.headLineStyle2.copyWith(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        RichText(text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '😍',
+                              style: TextStyle(fontSize: 38)
+                            ),
+                            TextSpan(
+                                text: '🥰',
+                                style: TextStyle(fontSize: 38)
+                            ),
+                            TextSpan(
+                                text: '😘',
+                                style: TextStyle(fontSize: 38)
+                            ),
+                          ]
+                        ))
+                      ],
+                    ),
+                  )
+                ],
               )
             ],
           )

f37aa23a9c4868d8618400c2e6a0b52e52e377de
sjLee <imy123123@naver.com>
2022-08-12 16:10:45 +0900

    [add: ticket_screen]  티켓 화면에서 결재 방법 보여주기

--- android/flutter/startBasic/lib/screens/ticket_screen.dart
+++ android/flutter/startBasic/lib/screens/ticket_screen.dart
@@ -25,15 +25,23 @@ class TicketScreen extends StatelessWidget {
                 vertical: AppLayout.getHeight(20)),
             children: [
               Gap(AppLayout.getHeight(40)),
-              Text('Tickets', style: Styles.headLineStyle1,),
+              Text(
+                'Tickets',
+                style: Styles.headLineStyle1,
+              ),
               Gap(AppLayout.getHeight(20)),
               const AppTicketTabs(firstTab: 'Upcoming', secondTab: 'Previous'),
               Gap(AppLayout.getHeight(20)),
               Container(
                 padding: EdgeInsets.only(left: AppLayout.getHeight(15)),
-                child: TicketView(ticket: ticketList[0], isColor: true,),
+                child: TicketView(
+                  ticket: ticketList[0],
+                  isColor: true,
+                ),
               ),
-              SizedBox(height: 1,),
+              SizedBox(
+                height: 1,
+              ),
               Container(
                 padding: EdgeInsets.all(15),
                 margin: EdgeInsets.symmetric(horizontal: 15),
@@ -56,10 +64,65 @@ class TicketScreen extends StatelessWidget {
                       ],
                     ),
                     Gap(AppLayout.getHeight(20)),
-                    const AppLayoutBuilderWidget(sections: 15, isColor: false, width: 5),
+                    const AppLayoutBuilderWidget(
+                        sections: 15, isColor: false, width: 5),
+                    Gap(AppLayout.getHeight(20)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        AppColumnLayout(
+                          firstText: '3654684 81312153',
+                          secondText: 'Number of E-ticket',
+                          alignment: CrossAxisAlignment.start,
+                        ),
+                        AppColumnLayout(
+                          firstText: 'B2SG28',
+                          secondText: 'Booking code',
+                          alignment: CrossAxisAlignment.end,
+                        )
+                      ],
+                    ),
+                    Gap(AppLayout.getHeight(20)),
+                    const AppLayoutBuilderWidget(
+                        sections: 15, isColor: false, width: 5),
+                    Gap(AppLayout.getHeight(20)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/visa.png",
+                                  scale: 11,
+                                ),
+                                Text(
+                                  " *** 2462",
+                                  style: Styles.headLineStyle3,
+                                )
+                              ],
+                            ),
+                            Gap(5),
+                            Text(
+                              "Payment method",
+                              style: Styles.headLineStyle4,
+                            )
+                          ],
+                        ),
+                        const AppColumnLayout(
+                          firstText: '\$249.99',
+                          secondText: 'Price',
+                          alignment: CrossAxisAlignment.end,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 1,),
+                    Gap(AppLayout.getHeight(20)),
                   ],
                 ),
-              )
+              ),
+
             ],
           )
         ],

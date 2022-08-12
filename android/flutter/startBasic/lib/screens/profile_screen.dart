886f597c5ca5b0ee52c0d24c4cadb83c253b4fd4
sjLee <imy123123@naver.com>
2022-08-12 16:56:56 +0900

    [add: profile_screen]  프로필 화면 상단 모양 구현

--- android/flutter/startBasic/lib/screens/profile_screen.dart
+++ android/flutter/startBasic/lib/screens/profile_screen.dart
@@ -1,5 +1,8 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:startbasic/utils/app_layout.dart';
 
 import '../utils/app_styles.dart';
 
@@ -11,8 +14,87 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
+        padding: EdgeInsets.all(AppLayout.getHeight(20)),
         children: [
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            // edit 이라고 쓰인 텍스트를 위쪽으로 정렬 시키기 위해서 사용한 것
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: AppLayout.getHeight(86),
+                width: AppLayout.getHeight(86),
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(AppLayout.getHeight(10)),
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/img_1.png"))),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Book Tickets",
+                    style: Styles.headLineStyle1,
+                  ),
+                  Gap(AppLayout.getHeight(2)),
+                  Text(
+                    "New-York",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey.shade500),
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(AppLayout.getHeight(100)),
+                        color: const Color(0xfffef4f3)),
+                    padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(3), vertical: AppLayout.getHeight(3)),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(3),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xff526799)),
+                          child: const Icon(
+                            FluentSystemIcons.ic_fluent_shield_filled,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        const Text("Premium status", style: TextStyle(
+                          color: Color(0xff526799), fontWeight: FontWeight.w600
+                        )),
+                        Gap(AppLayout.getHeight(5)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Column(
 
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print("you are tapped");
+                    },
+                  ),
+                  Text(
+                    "Edit",
+                    style: Styles.textStyle.copyWith(color: Styles.primaryColor, fontWeight: FontWeight.w300),
+
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );

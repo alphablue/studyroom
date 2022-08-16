4820b4f47e37b5651209362cdb099c623951ea37
sjLee <imy123123@naver.com>
2022-08-16 16:44:00 +0900

    [init]  개발을 위한 초기 세팅

---
+++ android/fast_30/android_compose/Lotto_app_part02_chapter02/app/src/main/java/com/example/lotto_app_part02_chapter02/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lotto_app_part02_chapter02
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lotto_app_part02_chapter02.ui.theme.Lotto_app_part02_chapter02Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}

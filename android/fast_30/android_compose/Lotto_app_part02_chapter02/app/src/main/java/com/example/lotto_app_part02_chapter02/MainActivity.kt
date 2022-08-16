0cdfa415c86ebe978592febb0bcd2df347de9084
sjLee <imy123123@naver.com>
2022-08-16 17:22:15 +0900

    [add function]  로또 숫자 생성 함수 개발

--- android/fast_30/android_compose/Lotto_app_part02_chapter02/app/src/main/java/com/example/lotto_app_part02_chapter02/MainActivity.kt
+++ android/fast_30/android_compose/Lotto_app_part02_chapter02/app/src/main/java/com/example/lotto_app_part02_chapter02/MainActivity.kt
@@ -3,20 +3,47 @@ package com.example.lotto_app_part02_chapter02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lotto_app_part02_chapter02.ui.theme.Lotto_app_part02_chapter02Theme
+import androidx.compose.ui.unit.dp
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            HomeView()
         }
     }
+}
+
+@Composable
+fun HomeView() {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()) {
+            val items = lottoRandomNumbers()
+            items.forEach {
+                Text(text = it.toString())
+                Spacer(modifier = Modifier.size(10.dp))
+            }
+        }
+    }
+}
+
+fun lottoRandomNumbers(): List<Int>{
+
+    val numbers = mutableSetOf<Int>()
+
+    while(numbers.size < 6) {
+        val randomNumber = Random.nextInt(1..45)
+        numbers.add(randomNumber)
+    }
+
+    return numbers.sorted()
 }

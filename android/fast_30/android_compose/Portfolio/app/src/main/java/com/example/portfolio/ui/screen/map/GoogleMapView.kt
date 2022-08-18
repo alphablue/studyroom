47dff5f9542668e09263a4c6401f9323cf75da4b
sjLee <imy123123@naver.com>
2022-08-18 16:38:52 +0900

    [error]  권한 설정의 순서 조절이 필요함

--- android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/screen/map/GoogleMapView.kt
+++ android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/screen/map/GoogleMapView.kt
@@ -1,14 +1,25 @@
 package com.example.portfolio.ui.screen.map
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
+import kotlinx.coroutines.launch
 
 @Composable
 fun GoogleMapView() {
@@ -16,9 +27,51 @@ fun GoogleMapView() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
+    val context = LocalContext.current
+    val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    var myLocation by remember {
+        mutableStateOf<Location?>(null)
+    }
+
+    val uiSettings = remember {MapUiSettings(myLocationButtonEnabled = true)}
+    val properties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true))}
+
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission Accepted: Do something
+            Log.d("ExampleScreen","PERMISSION GRANTED")
+
+        } else {
+            // Permission Denied: Do something
+            Log.d("ExampleScreen","PERMISSION DENIED")
+        }
+    }
+
+    LaunchedEffect(Unit ) {
+        launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        onMyLocationButtonClick = {
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            ) {
+//                buildAlertMessageNoGps(context)
+                Toast.makeText(context, "don't have gps", Toast.LENGTH_SHORT).show()
+                return@GoogleMap true
+            }
+            return@GoogleMap false
+
+        },
+        onMyLocationClick = {
+            myLocation = it
+            Toast.makeText(context, "long : ${myLocation?.longitude}, lat : ${myLocation?.latitude}", Toast.LENGTH_SHORT).show()
+        },
+        properties = properties,
+        uiSettings = uiSettings
     ) {
         Marker(
             state = MarkerState(position = singapore),
@@ -26,4 +79,5 @@ fun GoogleMapView() {
             snippet = "Marker in Singapore"
         )
     }
+
 }

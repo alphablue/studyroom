b7b7c9e0b4bef4ba9e9cfdf241cbd3b40069d2c1
sjLee <imy123123@naver.com>
2022-09-07 12:37:50 +0900

    [add firebaseCollections]  파이어 베이스에 데이터를 저장할 때 세부 컬렉션들을 추가함

--- android/flutter/blocstudy/lib/controllers/guestion_paper/data_uploader.dart
+++ android/flutter/blocstudy/lib/controllers/guestion_paper/data_uploader.dart
@@ -52,8 +52,22 @@ class DataUploader extends GetxController {
         "questions_count":paper.questions == null ?0:paper.questions!.length,
       });
 
-      print("check batch");
+      for(var questions in paper.questions!) {
+        final questionPath = questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          "question":questions.question,
+          "correct_answer":questions.correctAnswer
+        });
+        
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection("answers").doc(answer.identifier), {
+            "identifier": answer.identifier,
+            "answer": answer.answer
+          });
+        }
+      }
     }
+
     /// firebase 에 데이터를 게시하기 위한 부분 batch 에 데이터가 담겨 있는 상태이다.
     await batch.commit();
   }
--- android/flutter/blocstudy/lib/firebase_ref/references.dart
+++ android/flutter/blocstudy/lib/firebase_ref/references.dart
@@ -2,3 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 final fireStore = FirebaseFirestore.instance;
 final questionPaperRF = fireStore.collection('questionPaper');
+
+/// sub collection 을 생성해서 firestore 에 등록하는 방법을 보여주는 것임
+DocumentReference questionRF({
+  required String paperId,
+  required String questionId,
+}) => questionPaperRF.doc(paperId).collection("questions").doc(questionId);

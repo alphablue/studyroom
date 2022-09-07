8296544d1d1540e04bd68db53e2e65d9fede5844
sjLee <imy123123@naver.com>
2022-09-07 14:20:38 +0900

    [add splash, introduction]  스플레시 화면과 인트로 화면 작성

--- android/flutter/blocstudy/lib/main.dart
+++ android/flutter/blocstudy/lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:blocstudy/controllers/data_uploader_screen.dart';
 import 'package:blocstudy/firebase_options.dart';
+import 'package:blocstudy/screen/introduction/introduction.dart';
+import 'package:blocstudy/screen/splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-Future<void> main() async {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: AppIntroductionScreen(),
+    );
+  }
+}
+
+/*Future<void> main() async {
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(GetMaterialApp(home: DataUploaderScreen()));
-}
+}*/

f7febc3415bb9eb2aa5f4e844e406900a54e2dd4
sjLee <imy123123@naver.com>
2022-09-20 15:52:03 +0900

    [(portfolio) add theme]  테마 색상 적용

--- android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/theme/Color.kt
+++ android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Teal200 = Color(0xFF03DAC5)
 
 val yellow = Color(0xFFFFD300)
 val gray = Color(0xff808080)
+
+// blue & deepBlue
+val darkPrimaryBlue = Color(0xff0069c0)
+val lightPrimaryBlue = Color(0xff6ec6ff)
+val primaryBlue = Color(0xff2196f3)
+
+val secondaryBlue = Color(0xff1a237e)
+val lightSecondaryBlue = Color(0xff534bae)
+val darkSecondaryBlue = Color(0xff000051)
+
+val textColor = Color(0xffffffff)
+val accentBlue = Color(0xff00bcd4) // 강조색상
+val textPrimaryColor = Color(0xff212121)
+val textSecondaryColor = Color(0xff757575)
+val dividerColor = Color(0xffbdbdbd)
--- android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/theme/Theme.kt
+++ android/fast_30/android_compose/Portfolio/app/src/main/java/com/example/portfolio/ui/theme/Theme.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = darkPrimaryBlue,
+    primaryVariant = primaryBlue,
+    secondary = darkSecondaryBlue,
+    onSecondary = secondaryBlue,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = lightPrimaryBlue,
+    primaryVariant = primaryBlue,
+    secondary = secondaryBlue,
+    onSecondary = lightSecondaryBlue
 
     /* Other default colors to override
     background = Color.White,

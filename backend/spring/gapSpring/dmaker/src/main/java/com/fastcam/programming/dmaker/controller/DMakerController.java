f2c4bf87b672e3214f68943b76fb3a7689644d9b
sjLee <imy123123@naver.com>
2022-11-08 14:41:34 +0900

    [(BackEnd - dmaker) <validation> validation 작업]
     custom error code 정의와 validation 을 위한 작업 설정

--- backend/spring/gapSpring/dmaker/src/main/java/com/fastcam/programming/dmaker/controller/DMakerController.java
+++ backend/spring/gapSpring/dmaker/src/main/java/com/fastcam/programming/dmaker/controller/DMakerController.java
@@ -1,9 +1,12 @@
 package com.fastcam.programming.dmaker.controller;
 
+import com.fastcam.programming.dmaker.dto.CreateDeveloper;
 import com.fastcam.programming.dmaker.service.DMakerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -23,9 +26,11 @@ public class DMakerController {
         return Arrays.asList("snow", "Elsa", "Olaf");
     }
 
-    @GetMapping("/create-developer")
-    public List<String> createDevelopers() {
-        log.info("GET /create-developer HTTP/1.1");
+    @PostMapping("/create-developer")
+    public List<String> createDevelopers(
+            @RequestBody CreateDeveloper.Request request
+            ) {
+        log.info("request : {}", request);
 
         dMakerService.createDeveloper();
 
--- backend/spring/gapSpring/dmaker/src/main/java/com/fastcam/programming/dmaker/service/DMakerService.java
+++ backend/spring/gapSpring/dmaker/src/main/java/com/fastcam/programming/dmaker/service/DMakerService.java
@@ -1,6 +1,9 @@
 package com.fastcam.programming.dmaker.service;
 
+import com.fastcam.programming.dmaker.dto.CreateDeveloper;
 import com.fastcam.programming.dmaker.entity.Developer;
+import com.fastcam.programming.dmaker.exception.DMakerErrorCode;
+import com.fastcam.programming.dmaker.exception.DMakerException;
 import com.fastcam.programming.dmaker.repository.DeveloperRepository;
 import com.fastcam.programming.dmaker.type.DeveloperLevel;
 import com.fastcam.programming.dmaker.type.DeveloperSkillType;
@@ -8,13 +11,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static com.fastcam.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.fastcam.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
 
+    // ACID
+    // Atomic - 원자성
+    // Consistency - 일관성
+    // Isolation - 고립성
+    // Durability - 지속성 (모든 로그는 기록 해야 된다는 것)
+    // mysql 은 최근에 업데이트하면서 자동으로 로그를 남긴다.(durability 만족)
     @Transactional
-    public void createDeveloper() {
+    public void createDeveloper(
+            CreateDeveloper.Request request
+    ) {
+        validateCreateDeveloperRequest(request);
+
+
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
@@ -24,5 +43,28 @@ public class DMakerService {
                 .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent((developer -> {
+                    throw new DMakerException(DUPLICATED_MEMBER_ID);
+                }));
     }
 }

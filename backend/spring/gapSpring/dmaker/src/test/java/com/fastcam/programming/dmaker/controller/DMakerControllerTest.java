4d49266b3010afba6442ceab7ad1a197f5b609e4
sjLee <imy123123@naver.com>
2022-11-09 14:11:53 +0900

    [(BackEnd - dmaker) <test> junit, mockito]
     테스트 작업을 하는 방법 확인

--- backend/spring/gapSpring/dmaker/src/test/java/com/fastcam/programming/dmaker/controller/DMakerControllerTest.java
+++ backend/spring/gapSpring/dmaker/src/test/java/com/fastcam/programming/dmaker/controller/DMakerControllerTest.java
@@ -1,10 +1,13 @@
 package com.fastcam.programming.dmaker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest()
+@WebMvcTest(DMakerController.class)
 class DMakerControllerTest {
 
 }

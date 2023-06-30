72183b6532a07b56e46e6df75d54b7f150a6688f
LeeSJ <imy123123@naver.com>
2023-06-30 21:39:52 +0900

    (LeeSJ) 프로젝트 생성

---
+++ android/fast_35_with_compose/FastCampus35project_4_02/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FastCampus35project_4_02"
+include(":app")

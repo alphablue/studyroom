486b9ec6ad0686d0a959a82361a9090a7b428d2d
LeeSJ <imy123123@naver.com>
2023-07-02 20:43:05 +0900

    (LeeSJ) 뷰에 클릭 이벤트 data binding 방식으로 붙이기

--- android/fast_35_with_compose/FastCampus35project_4_02/app/src/main/java/com/example/fastcampus35project_4_02/widget/ShuffleNumberKeyboard.kt
+++ android/fast_35_with_compose/FastCampus35project_4_02/app/src/main/java/com/example/fastcampus35project_4_02/widget/ShuffleNumberKeyboard.kt
@@ -3,6 +3,7 @@ package com.example.fastcampus35project_4_02.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.view.children
@@ -10,12 +11,16 @@ import com.example.fastcampus35project_4_02.databinding.WidgetShuffleNumberKeybo
 import kotlin.random.Random
 
 class ShuffleNumberKeyboard @JvmOverloads
-constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : GridLayout(context, attrs, defStyleAttr) {
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : GridLayout(context, attrs, defStyleAttr), View.OnClickListener {
     private var _binding: WidgetShuffleNumberKeyboardBinding? = null
     private val binding get() = _binding!!
 
+    private var listener: KeyPadListener? = null
+
     init {
         _binding = WidgetShuffleNumberKeyboardBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.view = this
+        binding.clickListener = this
         shuffle()
     }
 
@@ -26,16 +31,40 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 
     fun shuffle() {
         val keyNumberArray = ArrayList<String>()
-        for(i in 0 .. 9) {
+        for (i in 0..9) {
             keyNumberArray.add(i.toString())
         }
 
-        binding.gridLayout.children.forEach {view ->
-            if(view is TextView && view.tag != null) {
+        binding.gridLayout.children.forEach { view ->
+            if (view is TextView && view.tag != null) {
                 val randIndex = Random.nextInt(keyNumberArray.size)
                 view.text = keyNumberArray[randIndex]
                 keyNumberArray.removeAt(randIndex)
             }
+        }
+    }
+
+    fun setKeyPadListener(keyPadListener: KeyPadListener) {
+        this.listener = keyPadListener
+    }
+
+    fun onClickDelete() {
+        listener?.onclickDelete()
+    }
+
+    fun onClickDone() {
+        listener?.onClickDone()
+    }
+
+    interface KeyPadListener {
+        fun onClickNum(num: String)
+        fun onclickDelete()
+        fun onClickDone()
+    }
+
+    override fun onClick(v: View) {
+        if(v is TextView && v.tag != null) {
+            listener?.onClickNum(v.text.toString())
         }
     }
 }

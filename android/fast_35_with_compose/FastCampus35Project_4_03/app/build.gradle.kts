83b14367b0f06d1b27722846e8d77f124960309b
seojin <imy123123@naver.com>
2023-07-18 03:54:19 +0900

    (LeeSJ) 카드뷰 레이아웃 완성 및 데이터 바인딩 연결

--- android/fast_35_with_compose/FastCampus35Project_4_03/app/build.gradle.kts
+++ android/fast_35_with_compose/FastCampus35Project_4_03/app/build.gradle.kts
@@ -1,7 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +33,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    dataBinding {
+        enable = true
     }
 }
 

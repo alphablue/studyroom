6f155c00ce090669a202f70b0f6b673428b94f82
LeeSJ <imy123123@naver.com>
2024-07-11 17:04:01 +0900

    (LeeSj) [CommonUI::메인 구성요소] 1. 기본 UI 학습을 위한 환경 구축 완료

--- android/StudyReference/architectmodule/src/main/java/com/study/architectmodule/MainArchitectActivity.kt
+++ android/StudyReference/architectmodule/src/main/java/com/study/architectmodule/MainArchitectActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.study.architectmodule.ui.theme.StudyReferenceTheme
+import jini.text.CountingTextView
 
 class MainArchitectActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +22,14 @@ class MainArchitectActivity : ComponentActivity() {
         setContent {
             StudyReferenceTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
+                        CountingTextView()
+                    }
                 }
             }
         }
---
+++ android/StudyReference/studyFeature/CommonUI/src/main/kotlin/jini/text/TextAnimationGroup.kt
@@ -0,0 +1,9 @@
+package jini.text
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CountingTextView() {
+    Text(text = "Common UI module")
+}

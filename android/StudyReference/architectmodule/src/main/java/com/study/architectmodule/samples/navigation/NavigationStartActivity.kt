91a60d550ebefc26658fa526c8a2df5ae38c795f
LeeSJ <imy123123@naver.com>
2025-04-08 10:58:47 +0900

    학습(LeeSj): 강의 중단 -- part5-2 좋아요 기능 구현 파트까지
    UI_정보: [프로필]
    1. 강의에서 더 이상 코드를 통해서 팁, 학습 내용들이 보이지 않음 자체 설계하고 충분히 학습 할 수 있을뜻
    
    참조 :

---
+++ android/StudyReference/architectmodule/src/main/java/com/study/architectmodule/samples/navigation/NavigationStartActivity.kt
@@ -0,0 +1,16 @@
+package com.study.architectmodule.samples.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class NavigationStartActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
--- android/StudyReference/studyFeature/fastThirtyFive-domain/src/main/java/com/example/fastthirtyfive_domain/repository/ThirtyFiveSearchRepository.kt
+++ android/StudyReference/studyFeature/fastThirtyFive-domain/src/main/java/com/example/fastthirtyfive_domain/repository/ThirtyFiveSearchRepository.kt
@@ -10,3 +10,39 @@ interface ThirtyFiveSearchRepository {
 
     fun getSearchKeywords(): Flow<List<ThirtyFiveSearchKeyword>>
 }
+
+//interface Destination {
+//    val route: String
+//    val title: String
+//    val deepLinks: List<NavDeepLink>  // android lib
+//}
+//
+//interface DestinationArg<T>: Destination {
+//    val argName: String
+//    val arguments: List<NamedNavArgument>  // android lib
+//
+//    fun routeWithArgName() = "$route/{$arguments}"
+//    fun navigateWithArg(itme: T): String
+//    fun findArgument(navBackStackEntry: NavBackStackEntry) : T?  // android lib
+//}
+//
+//object CategoryNav: DestinationArg<ThirtyFiveCategory> {
+//    override val route: String = "category"
+//    override val title: String = "카테고리"
+//    override val argName: String = "category"
+//    override val arguments: List<NamedNavArgument> = listOf(
+//        navDeepLink { uriPattern = "$scheme$route/{$argName}"}
+//    )
+//
+//    override val deepLinks: List<NavDeepLink> = navDeepLink { uriPattern = "$scheme$route/{$argName}"}
+//
+//    override fun findArgument(navBackStackEntry: NavBackStackEntry): ThirtyFiveCategory? {
+//        val categoryString = navBackStackEntry.arguments?.getString(argName)
+//        return GsonUtils.fromJson<ThirtyFiveCategory>
+//    }
+//
+//    override fun navigateWithArg(itme: ThirtyFiveCategory): String {
+//        val arg = GsonUtils.toJson(item)
+//        return "$route/{$arg}"
+//    }
+//}

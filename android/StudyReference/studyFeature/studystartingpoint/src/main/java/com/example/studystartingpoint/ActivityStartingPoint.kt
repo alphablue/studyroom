2b52922d3a118be02a5ce60cb98af19fe825faeb
LeeSJ <imy123123@naver.com>
2025-04-22 17:28:43 +0900

    학습(LeeSj): 텍스트 커스터 마이징을 위한 자료 수집중 (error)
    UI_정보: [텍스트]
    1. 텍스트의 커스터 마이징과 layout 의 이해
    
    참조 :

--- android/StudyReference/studyFeature/studystartingpoint/src/main/java/com/example/studystartingpoint/ActivityStartingPoint.kt
+++ android/StudyReference/studyFeature/studystartingpoint/src/main/java/com/example/studystartingpoint/ActivityStartingPoint.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,17 +24,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withAnnotation
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import com.example.studystartingpoint.challengeUi.text.customViewCheck
 import com.example.studystartingpoint.ui.theme.Purple
 import com.example.studystartingpoint.ui.theme.StudyReferenceTheme
 import com.example.studystartingpoint.util.d
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+import kotlin.math.PI
+import kotlin.math.ceil
+import kotlin.math.sin
+import kotlin.time.Duration.Companion.seconds
 
 class ActivityStartingPoint : ComponentActivity() {
     @OptIn(ExperimentalTextApi::class)
@@ -59,7 +74,7 @@ class ActivityStartingPoint : ComponentActivity() {
                             val textAnnotation = buildAnnotatedString {
                                 append("텍스트 입니다.")
                                 withAnnotation("태그", annotation = "어노테이션") {
-                                    withStyle(SpanStyle(color = Purple)) {
+                                    withStyle(SpanStyle(color = Purple, fontSize = 24.sp)) {
                                         append("냠냠냠 맛있다.") // 8글자
                                     }
                                 }
@@ -73,7 +88,7 @@ class ActivityStartingPoint : ComponentActivity() {
                                 onTextLayout = { textLayout ->
 
                                     // stringBuilder 에서 annotation을 정의한 값을 가져온다. 만들어진 글자의 index 중 start 와 end 사이에 tag에 해당하는 내용이 있다면 추출된다. 없다면 크래시
-                                    val annotation = textAnnotation.getStringAnnotations("태그", 0, 999).let {
+                                    val annotation = textAnnotation.getStringAnnotations("태그", 0, textAnnotation.lastIndex).let {
                                         it.forEach { list ->
                                             list.toString().d("customText")
                                         }
@@ -93,11 +108,27 @@ class ActivityStartingPoint : ComponentActivity() {
 
                                     onDraw = {
                                         for (bound in textBounds) {
-                                            val underline = bound.copy(top = bound.bottom - 4.sp.toPx())
-                                            drawRect(
-                                                color = Purple,
-                                                topLeft = underline.topLeft,
-                                                size = underline.size
+                                            val underline = bound.copy(top = bound.bottom * 0.93f) // top 을 bottom 에서 일정 비율만큼 빼도록 하기 위한 값 설정, bottom - top 이 size 에서 height 값이 된다.
+
+                                            // span style
+                                            val spanStyle = textAnnotation.spanStyles.first { range -> maxOf(annotation.start, range.start) < minOf(annotation.end, range.end) }
+                                            "first: ${spanStyle.item.fontSize}".d("checkSize")
+
+                                            "bound: $bound, underlineBound: $underline".d("checkSize")
+                                            "offset (top) : ${bound.topLeft} , ${bound.topCenter} , ${bound.topRight}".d("checkSize")
+                                            "offset (center) : ${bound.centerLeft} , ${bound.center} , ${bound.centerRight}".d("checkSize")
+                                            "offset (bottom) : ${bound.bottomLeft} , ${bound.bottomCenter} , ${bound.bottomRight}".d("checkSize")
+
+//                                            drawRect(
+//                                                color = Purple,
+//                                                topLeft = underline.topLeft,
+//                                                size = underline.size // width , height 값을 반환
+//                                            )
+
+                                            drawPath(
+                                                path = Path(
+
+                                                )
                                             )
                                         }
                                     }
@@ -109,4 +140,62 @@ class ActivityStartingPoint : ComponentActivity() {
             }
         }
     }
+}
+
+private val TWO_PI = 2 * PI.toFloat()
+private val SEGMENTS_PER_WAVELENGTH = 10
+
+private fun Path.buildSquigglesFor(
+    box: Rect,
+    density: Density,
+    width: TextUnit = 4.sp,
+    wavelength: TextUnit = 10.sp,
+    amplitude: TextUnit = 10.sp,
+    bottomOffset: TextUnit = 10.sp
+) = density.run {
+    val lineStart = box.left + (width.toPx() / 2)
+    val lineEnd = box.right - (width.toPx() / 2)
+    val lineBottom = box.bottom + bottomOffset.toPx()
+
+    val segmentWidth = wavelength.toPx() / SEGMENTS_PER_WAVELENGTH
+    val numOfPoints = ceil((lineEnd - lineStart) / segmentWidth).toInt() + 1
+
+    val animationProgress = rememberInfiniteTransition().animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1.seconds.inWholeMilliseconds.toInt(), easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        )
+    )
+    remember {
+        SquigglyUnderlineAnimator(animationProgress)
+    }
+
+    var pointX = lineStart
+    fastMapRange(0, numOfPoints) { point ->
+        val proportionOfWavelength = (pointX - lineStart) / wavelength.toPx()
+        val radiansX = proportionOfWavelength * TWO_PI + (TWO_PI * animator.animationProgress.value)
+        val offsetY = lineBottom + (sin(radiansX) * amplitude.toPx())
+
+        when (point) {
+            0 -> moveTo(pointX, offsetY)
+            else -> lineTo(pointX, offsetY)
+        }
+        pointX = (pointX + segmentWidth).coerceAtMost(lineEnd)
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun <R> fastMapRange(
+    start: Int,
+    end: Int,
+    transform: (Int) -> R
+): List<R> {
+    contract { callsInPlace(transform) }
+    val destination = ArrayList<R>(end - start + 1)
+    for(i in start..end) {
+        destination.add(transform(i))
+    }
+    return destination
 }
